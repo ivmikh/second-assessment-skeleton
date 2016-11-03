04cd41abc2e423f20378665c3b9cc1c2a8c7184f
ivmikh <ivan.mikhaylov@gmail.com>
2016-11-03 08:25:54 -0500

    Initial project skeleton

---
+++ src/main/java/cooksys/SecondAssesmentApplication.java
@@ -0,0 +1,12 @@
+package cooksys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondAssesmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondAssesmentApplication.class, args);
+	}
+}
